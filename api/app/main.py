2581fc3a1c49826832761750f92e50d5ca4f72f3
Henrycke Bozza Schenberk <0Kerryhen@pm.me>
2025-09-13 12:12:18 -0300

    vercel update

--- api/app/main.py
+++ api/app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+# from fastapi.staticfiles import StaticFiles
+# from fastapi.responses import FileResponse, HTMLResponse
 
 from .routers import auth, users
 from .settings import Settings
@@ -10,9 +12,28 @@ app = FastAPI()
 app.include_router(users.router)
 app.include_router(auth.router)
 
-@app.get("/")
-async def health_check():
-    return "The health check is successful!"
+# app.mount("/public", StaticFiles(directory="public"), name="public")
+
+
+# @app.get("/favicon.ico", include_in_schema=False)
+# async def favicon():
+#     return FileResponse("public/favicon.ico")
+
+
+# @app.get("/", response_class=HTMLResponse)
+# def read_root():
+#     return """
+#     <!DOCTYPE html>
+#     <html>
+#     <head>
+#         <title>Vercel + FastAPI</title>
+#         <link rel="icon" type="image/x-icon" href="/favicon.ico">
+#     </head>
+#     <body>
+#         <h1>Vercel + FastAPI</h1>
+#     </body>
+#     </html>
+#     """
 
 app.add_middleware(
     CORSMiddleware,
