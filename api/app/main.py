42027ec9a0947e051b4180d5b59ebf3e1a80bd60
Henrycke Bozza Schenberk <0Kerryhen@pm.me>
2025-09-13 09:03:57 -0300

    vercel test

--- api/app/main.py
+++ api/app/main.py
@@ -10,7 +10,10 @@ app = FastAPI()
 app.include_router(users.router)
 app.include_router(auth.router)
 
-print(settings.ORIGINS)
+@app.get("/")
+async def health_check():
+    return "The health check is successful!"
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.ORIGINS,  # ou ["*"] para liberar tudo (não recomendado em produção)
